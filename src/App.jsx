dbe0c26a50ea45edda6f289e10d86112b8dbbcd7
Rahul <rahulkumargupta9326@gmail.com>
2026-07-14 21:22:33 +0530

    add NewsLetter and Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route,Routes, useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
-   <>
-   {!isOwnerPath && <Navbar/>}
-   <div className='min-h-[70vh]'>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
+    <>
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
 
-   </div>
-   
-   
-   </>
+
+      </div>
+      <Footer />
+
+    </>
   )
 }
 
