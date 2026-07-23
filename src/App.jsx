efa5dcc936f8c7d3b44a5123e07014554b293420
Rahul <rahulkumargupta9326@gmail.com>
2026-07-23 21:42:54 +0530

    add rooms and filter

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import AllRooms from './pages/AllRooms';
 
 
 
@@ -15,6 +16,7 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<AllRooms/>} />
         </Routes>
 
 
